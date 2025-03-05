7e331d8a7f2fed32e53cb6fbe3c4076009fdd18e
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2025-03-05 13:18:15 -0300

    urls.py

--- diario/urls.py
+++ diario/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login/', views.login_usuario, name='login'),
     path('entrada/', views.criar_entrada, name='entrada_diario'),
     path('entradas/', views.exibir_entradas, name='exibir_entradas'),
+    path("editar/<int:entrada_id>/", views.editar_entrada, name="editar_entrada"),
+    path("excluir/<int:entrada_id>/", views.excluir_entrada, name="excluir_entrada"),
 ]
